432de61cfc03d96ecf9ff432bdce9ed6e3b76dcd
Mike Solomon <mike@meeshkan.com>
2024-03-01 01:08:50 +0200

    mask

--- train_audio.py
+++ train_audio.py
@@ -50,7 +50,7 @@ wandb_run_name = 'gpt2' # 'run' + str(time.time())
 # data
 dataset = 'audio'
 gradient_accumulation_steps = 5 * 8 # used to simulate larger batch sizes
-batch_size = 12 # if gradient_accumulation_steps > 1, this is the micro-batch size
+batch_size = 8 # if gradient_accumulation_steps > 1, this is the micro-batch size
 block_size = 1024
 # model
 n_layer = 2
