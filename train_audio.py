f1174d5651b2168f45d532ecfba4fd7ebf5f56da
Mike Solomon <mike@meeshkan.com>
2024-03-01 00:47:49 +0200

    mask

--- train_audio.py
+++ train_audio.py
@@ -51,7 +51,7 @@ batch_size = 12 # if gradient_accumulation_steps > 1, this is the micro-batch si
 block_size = 1024
 # model
 n_layer = 4
-n_head = 12
+n_head = 8
 n_embd = 768
 dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
 bias = False # do we use bias inside LayerNorm and Linear layers?
